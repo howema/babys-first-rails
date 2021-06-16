85abca65ee7b5b6d2b696c7fb4a7e8270e50064d
MEG HOWE <howem9@gmail.com>
2021-06-15 22:57:27 -0400

    adds today's homework

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,9 @@
+class WelcomesController < ApplicationController
+  def hello_method
+    render json: { message: "Hello from welcomes controller!" }
+  end
+
+  def about_method
+    render json: { message: "My favorite language is ruby. It looks like a wedge!" }
+  end
+end
